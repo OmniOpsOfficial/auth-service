fbdb8dbb082e92393bc3b7cc4142de4430b7c9c4
ajweh77 <mohammad.ajweh@omniops.me>
2024-12-01 14:37:48 +0300

    init

--- src/main/java/me/omniops/security/OIDCExceptionMapper.java
+++ src/main/java/me/omniops/security/OIDCExceptionMapper.java
@@ -1,20 +1,44 @@
 package me.omniops.security;
 
 import io.quarkus.oidc.OIDCException;
+import io.quarkus.security.AuthenticationFailedException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 import lombok.extern.slf4j.Slf4j;
-import org.jboss.logging.Logger;
 
 @Slf4j
 @Provider
 public class OIDCExceptionMapper implements ExceptionMapper<OIDCException> {
+
+
     @Override
     public Response toResponse(OIDCException exception) {
-       log.error("OIDC exception occurred: {} " , exception.getMessage());
-        return Response.status(Response.Status.UNAUTHORIZED)
-                .entity("Token-related error: " + exception.getMessage())
-                .build();
+        String message = "Authentication error occurred.";
+
+        Throwable cause = exception.getCause();
+        if (cause != null) {
+            String causeMessage = cause.getMessage();
+            if (causeMessage != null) {
+                if (causeMessage.contains("expired")) {
+                    message = "Token has expired.";
+                } else if (causeMessage.contains("invalid")) {
+                    message = "Token is invalid.";
+                } else if (causeMessage.contains("issuer")) {
+                    message = "Token issuer is incorrect.";
+                }
+            }
+        }
+        return Response.status(Response.Status.UNAUTHORIZED).entity(message).build();
     }
+
+//    @Override
+//    public Response toResponse(OIDCException exception) {
+//       log.error("OIDC exception occurred: {} " , exception.getMessage());
+//
+//        return Response.status(Response.Status.UNAUTHORIZED)
+//                .entity("Token-related error: " + exception.getMessage())
+//                .build();
+//
+//    }
 }
--- src/main/java/me/omniops/users/UserService.java
+++ src/main/java/me/omniops/users/UserService.java
@@ -1,12 +1,25 @@
 package me.omniops.users;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import me.omniops.security.Role;
+import me.omniops.security.UserInfo;
 import org.keycloak.admin.client.resource.UserResource;
+import org.keycloak.representations.idm.UserRepresentation;
+
+import java.util.List;
 
 @ApplicationScoped
 public class UserService extends KeycloakConnection {
 
     public String getUserById(String userId) {
         return getRealmConnection().users().get(userId).toRepresentation().getEmail();
+    }
+
+    public List<UserInfo> getDetails(Role role){
+        return getRealmConnection().roles().get(role.name()).getUserMembers().stream().map(this::toUserInfo).toList();
+    }
+
+    private UserInfo toUserInfo(UserRepresentation user) {
+       return new UserInfo(user.getId(), user.getUsername(), user.getFirstName(), user.getLastName(), user.getEmail(), null,null,null,null);
     }
 }
